a2099edacbbaab5be2b2ee6716df990e3658a843
Dsolnik <danielsolnik@yahoo.com>
2016-01-10 22:00:23 -0500

    Solved a few more problems

--- Project Euler Problem 1/Project Euler Problem 1/Source.cpp
+++ Project Euler Problem 1/Project Euler Problem 1/Source.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main()
 {
-
+	int sum = 0;
+	for (int i = 1; i < 1000; i++)
+	{
+		if (i % 3 == 0 || i%5 == 0)
+		{
+			sum += i;
+		}
+	}
+	cout << sum << endl;
+	system("PAUSE");
 }
